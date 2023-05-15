b958b9d808e4bb8be9f3f44510579a4874a4facd
qwli7 <228794754@qq.com>
2023-05-15 23:50:11 +0800

    PySide example

--- HelloWorld/hello_world.py
+++ HelloWorld/hello_world.py
@@ -1,5 +1,5 @@
 import sys
-from PySide6.QtWidgets import QApplication, QLabel
+from PyQt5.QtWidgets import QApplication, QLabel
 
 app = QApplication(sys.argv)
 label = QLabel('hello world')
---
+++ QLabel/readme.md
@@ -0,0 +1,11 @@
+## QLabel
+一个简单的标签控件，常用来显示文本，超链接，或者图片。
+
+## 使用方式
+```python
+label = QLabel()
+label.setText('标签')
+```
+以下为常用的属性
+- alignment
+  - 
